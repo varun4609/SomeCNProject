a3861a6315936b0538bb89865e0f4d956c10136a
vfrost <vfrost@10.136.24.34>
2018-10-18 20:17:38 -0400

    formatting shit

--- src/p2p/Client.java
+++ src/p2p/Client.java
@@ -7,84 +7,76 @@ import java.nio.channels.*;
 import java.util.*;
 
 public class Client {
-	Socket requestSocket;           //socket connect to the server
-	ObjectOutputStream out;         //stream write to the socket
-	ObjectInputStream in;          //stream read from the socket
-	String message;                //message send to the server
-	String MESSAGE;                //capitalized message read from the server
+	Socket requestSocket; // socket connect to the server
+	ObjectOutputStream out; // stream write to the socket
+	ObjectInputStream in; // stream read from the socket
+	String message; // message send to the server
+	String MESSAGE; // capitalized message read from the server
 	FileOutputStream fr;
 
-	public Client() {}
+	public Client() {
+	}
 
-	void run()
-	{
-		try{
-			//create a socket to connect to the server
+	void run() {
+		try {
+			// create a socket to connect to the server
 			requestSocket = new Socket("localhost", 8000);
 			System.out.println("Connected to localhost in port 8000");
-			//initialize inputStream and outputStream
+			// initialize inputStream and outputStream
 			out = new ObjectOutputStream(requestSocket.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(requestSocket.getInputStream());
-			
 
-			//get Input from standard input
+			// get Input from standard input
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			byte []b=new byte[3000];
-			fr=new FileOutputStream("C:\\Users\\Varun\\eclipse-workspace\\p2p\\src\\p2p\\t.txt");
-			while(true)
-			{
-				//System.out.print("Hello, please input a sentence: ");
-				//read a sentence from the standard input
-				//message = bufferedReader.readLine();
-				//Send the sentence to the server
-				//sendMessage(message);
-				//Receive the upperCase sentence from the server
+			byte[] b = new byte[3000];
+			fr = new FileOutputStream("C:\\Users\\Varun\\eclipse-workspace\\p2p\\src\\p2p\\t.txt");
+			while (true) {
+				// System.out.print("Hello, please input a sentence: ");
+				// read a sentence from the standard input
+				// message = bufferedReader.readLine();
+				// Send the sentence to the server
+				// sendMessage(message);
+				// Receive the upperCase sentence from the server
 				in.read(b, 0, b.length);
-				
+
 				fr.write(b, 0, b.length);
 				message = bufferedReader.readLine();
-				//show the message to the user
-				//System.out.println("Receive message: " + MESSAGE);
+				// show the message to the user
+				// System.out.println("Receive message: " + MESSAGE);
 			}
-		}
-		catch (ConnectException e) {
+		} catch (ConnectException e) {
 			System.err.println("Connection refused. You need to initiate a server first.");
-		} 
-		catch(UnknownHostException unknownHost){
+		} catch (UnknownHostException unknownHost) {
 			System.err.println("You are trying to connect to an unknown host!");
-		}
-		catch(IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
-		}
-		finally{
-			//Close connections
-			try{
+		} finally {
+			// Close connections
+			try {
 				fr.close();
 				in.close();
 				out.close();
 				requestSocket.close();
-			}
-			catch(IOException ioException){
+			} catch (IOException ioException) {
 				ioException.printStackTrace();
 			}
 		}
 	}
-	//send a message to the output stream
-	void sendMessage(String msg)
-	{
-		try{
-			//stream write the message
+
+	// send a message to the output stream
+	void sendMessage(String msg) {
+		try {
+			// stream write the message
 			out.writeObject(msg);
 			out.flush();
-		}
-		catch(IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
 	}
-	//main method
-	public static void main(String args[])
-	{
+
+	// main method
+	public static void main(String args[]) {
 		Client client = new Client();
 		client.run();
 	}
--- src/p2p/Server.java
+++ src/p2p/Server.java
@@ -8,36 +8,36 @@ import java.util.*;
 
 public class Server {
 
-	private static final int sPort = 8000;   //The server will be listening on this port number
+	private static final int sPort = 8000; // The server will be listening on this port number
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("The server is running."); 
+		System.out.println("The server is running.");
 		ServerSocket listener = new ServerSocket(sPort);
 		int clientNum = 1;
 		try {
-			while(true) {
-				new Handler(listener.accept(),clientNum).start();
-				System.out.println("Client "  + clientNum + " is connected!");
+			while (true) {
+				new Handler(listener.accept(), clientNum).start();
+				System.out.println("Client " + clientNum + " is connected!");
 				clientNum++;
 			}
 		} finally {
 			listener.close();
-		} 
+		}
 
 	}
 
 	/**
-	 * A handler thread class.  Handlers are spawned from the listening
-	 * loop and are responsible for dealing with a single client's requests.
+	 * A handler thread class. Handlers are spawned from the listening loop and are
+	 * responsible for dealing with a single client's requests.
 	 */
 	private static class Handler extends Thread {
-		private String message;    //message received from the client
-		private String MESSAGE;    //uppercase message send to the client
+		private String message; // message received from the client
+		private String MESSAGE; // uppercase message send to the client
 		private Socket connection;
-		private ObjectInputStream in;	//stream read from the socket
-		private ObjectOutputStream out;    //stream write to the socket
+		private ObjectInputStream in; // stream read from the socket
+		private ObjectOutputStream out; // stream write to the socket
 		FileInputStream fis;
-		private int no;		//The index number of the client
+		private int no; // The index number of the client
 
 		public Handler(Socket connection, int no) {
 			this.connection = connection;
@@ -45,66 +45,59 @@ public class Server {
 		}
 
 		public void run() {
-			try{
-				//initialize Input and Output streams
+			try {
+				// initialize Input and Output streams
 				out = new ObjectOutputStream(connection.getOutputStream());
 				out.flush();
 				int i = 1;
 				in = new ObjectInputStream(connection.getInputStream());
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-				try{
+				try {
 					fis = new FileInputStream("C:\\Users\\Varun\\eclipse-workspace\\p2p\\src\\p2p\\test.txt");
 					byte b[] = new byte[3000];
 					fis.read(b, 0, b.length);
-					
-					int chunkSize = (int)Math.ceil(b.length / 100);
+
+					int chunkSize = (int) Math.ceil(b.length / 100);
 					int currPtr = 0;
-					while(currPtr < b.length)
-					{
-						//receive the message sent from the client
-						//message = (String)in.readObject();
-						
-						//show the message to the user
+					while (currPtr < b.length) {
+						// receive the message sent from the client
+						// message = (String)in.readObject();
+
+						// show the message to the user
 						System.out.println("Sending message: " + (currPtr + chunkSize));
-						//Capitalize all letters in the message
-						//MESSAGE = Integer.toString(i);
-						//send MESSAGE back to the client
+						// Capitalize all letters in the message
+						// MESSAGE = Integer.toString(i);
+						// send MESSAGE back to the client
 						sendMessage(b, currPtr, chunkSize);
 						message = bufferedReader.readLine();
 						currPtr += chunkSize;
 						i++;
 					}
-				}
-				catch(Exception e){
+				} catch (Exception e) {
 					System.err.println("Data received in unknown format");
 				}
-			}
-			catch(IOException ioException){
+			} catch (IOException ioException) {
 				System.out.println("Disconnect with Client " + no);
-			}
-			finally{
-				//Close connections
-				try{
+			} finally {
+				// Close connections
+				try {
 					fis.close();
 					in.close();
 					out.close();
 					connection.close();
-				}
-				catch(IOException ioException){
+				} catch (IOException ioException) {
 					System.out.println("Disconnect with Client " + no);
 				}
 			}
 		}
 
-		//send a message to the output stream
-		public void sendMessage(byte[] msg, int start, int end)
-		{
-			try{
+		// send a message to the output stream
+		public void sendMessage(byte[] msg, int start, int end) {
+			try {
 				out.write(msg, start, end);
 				out.flush();
 				System.out.println("Send message: " + start + " to " + end);
-			}
-			catch(IOException ioException){
+			} catch (IOException ioException) {
 				ioException.printStackTrace();
 			}
 		}
